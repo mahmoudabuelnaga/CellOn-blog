5c5a50904f212f6756ec21a3204363eea99b9da7
mahmoudaboelnaga <m.aboelnaga@myonekey.com>
2020-04-18 19:52:22 +0200

    add files

--- blog/models.py
+++ blog/models.py
@@ -4,29 +4,30 @@ from django.utils.text import slugify
 from django.urls import reverse
 import datetime
 # Create your models here.
+
+
 class Blog(models.Model):
-    user    = models.ForeignKey(User , on_delete=models.CASCADE)
-    title   = models.CharField(max_length=100)
-    slug    = models.SlugField(blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(blank=True, null=True)
     content = models.TextField()
     publish = models.DateTimeField(default=datetime.datetime.now, blank=True)
-    # img     = models.ImageField( upload_to='uploads')
+    img = models.ImageField(upload_to='uploads')
 
     def __str__(self):
         return self.title
-    
+
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.title)
         super(Blog, self).save(*args, **kwargs)
 
-    def get_absolute_url(self): # new
+    def get_absolute_url(self):  # new
         return reverse('detail', args=[str(self.slug)])
 # class Post(models.Model):
 #     title = models.CharField(max_length=200)
 #     author = models.ForeignKey('auth.User',on_delete=models.CASCADE)
 #     body = models.TextField()
-    
+
 #     def __str__(self):
 #         return self.title
-    
